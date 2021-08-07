9b712dd740f69e882782392165c1f3a1583c02f5
ChiaChi-Chang <cyhs211010@gmail.com>
2021-08-07 22:38:09 +0800

    20210807

--- src/views/_Chi.vue
+++ src/views/_Chi.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>Chi</h1>
+    <RwdNavbar></RwdNavbar>
 </template>
+
+
+<script>
+import RwdNavbar from "../components/_Chi/RwdNavbar.vue"
+export default {
+    components:{
+        RwdNavbar
+    }
+}
+</script>
