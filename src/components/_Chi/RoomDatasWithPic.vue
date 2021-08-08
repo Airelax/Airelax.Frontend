badc859783f5b4c3d1e2386348e282a399060af0
ChiaChi-Chang <cyhs211010@gmail.com>
2021-08-08 14:01:23 +0800

    20210808

---
+++ src/components/_Chi/RoomDatasWithPic.vue
@@ -0,0 +1,80 @@
+<template>
+        <div class="cardTemplate">
+            <h2>住宿地點</h2>
+            <div class="rooms">
+                <div class="roomCards" v-for="(room, index) in roomDatas" :key="room.id">
+                    <div class="card" style="width: 18rem;">
+                        <img :src="room.picture" class="card-img-top roomPic" alt="...">
+                        <div class="card-body">
+                            臥室{{index+1}}
+                            <p class="card-text2">{{room.BedCount}}張{{room.BedType}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    data:function(){
+        return{
+            roomDatas:[],
+            
+        }
+    },
+    create(){
+        
+    },
+    methods:{
+
+    },
+    mounted:function(){
+        axios
+            .get(
+                "https://raw.githubusercontent.com/Airelax/Airelax.Frontend/master/project/fake-room-data.json"
+            )
+            .then((response)=>{
+                console.log(response.data)
+                this.roomDatas=response.data[0].BedroomDetail
+                
+            })
+    },
+}
+</script>
+
+<style scoped lang="scss">
+.cardTemplate{
+    padding: 48px 0;
+    .rooms{
+        padding: auto;
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        overflow: hidden;
+        
+        .roomCards{
+            padding: 8px;
+            
+            
+            
+            .card{
+                cursor: pointer;
+                border:none;
+                width: 100%;
+                .roomPic{
+                    width: 100%;
+                    border-top-right-radius: 10px;
+                    border-top-left-radius: 10px;
+                }
+
+                .card-body{
+                    
+                }
+            }
+        }
+    }
+}
+
+</style>
--- src/views/_Chi.vue
+++ src/views/_Chi.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>Chi</h1>
+    <beds></beds>
 </template>
+
+<script>
+import beds from "../components/_Chi/RoomDatasWithPic.vue"
+export default {
+    components:{
+        beds
+    }
+}
+</script>
