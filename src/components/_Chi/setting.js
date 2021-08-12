850f0032417fa339dcda4b8da9a2b4cce96a6a5d
ChiaChi-Chang <cyhs211010@gmail.com>
2021-08-12 21:56:06 +0800

    123

---
+++ src/components/_Chi/setting.js
@@ -0,0 +1,39 @@
+export default {
+    //Honor
+    honor: [
+        {
+            img: require('圖片位置'),
+            chinese: '深度清潔',
+            description: '此房東承諾遵守 Airbnb 的深度清潔五步驟。'
+        },
+        //依此類推
+    ],
+
+    //設施
+    facilities: [
+        {
+            name: 'HairDryer',
+            chinese: '吹風機',
+            mapping: 106,
+            img: require('圖片位置')
+        },
+        //依此類推
+    ],
+
+    //房屋守則
+    houseRules: [
+        {
+            chinese: '入住時間',
+            img: require('圖片位置')
+        },
+        //依此類推
+    ],
+
+    //健康與安全
+    safety: [
+        {
+            chinese: '入住時間',
+            img: require('圖片位置')
+        },
+    ]
+}
